609b1fe2e975a69616df35b8b3094d188ff02304
DannyCeacFp <danny.cuenca@alu.ceacfp.com>
2023-05-26 12:27:25 +0200

    Creando clase en paquete diferente

---
+++ ExamenGit/src/paquete2/ClaseDos.java
@@ -0,0 +1,14 @@
+package paquete2;
+
+public class ClaseDos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String Bienvenida="Hola Bienvenido";
+		 
+		
+		
+	}
+
+}
