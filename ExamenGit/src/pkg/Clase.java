2729270f4109f0e548a5f5b89c057101ad0285de
DannyCeacFp <danny.cuenca@alu.ceacfp.com>
2023-05-26 12:22:43 +0200

    Agregando saludo a rama1

---
+++ ExamenGit/src/pkg/Clase.java
@@ -0,0 +1,16 @@
+package pkg;
+
+public class Clase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String saludo="Hola";
+		int numero=5;
+		int numeroAleatorio=65;
+		
+		
+
+	}
+
+}
