fc4123a0ed1915bbc4eca63cb4985b44f91a7f81
DannyCeacFp <danny.cuenca@alu.ceacfp.com>
2023-05-26 12:31:51 +0200

    modificando saludo

--- ExamenGit/src/pkg/Clase.java
+++ ExamenGit/src/pkg/Clase.java
@@ -5,7 +5,7 @@ public class Clase {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String saludo="Hola como te encuentras";
+		String saludo="Hola como te encuentras espero que bien";
 		int numero=553453;
 		int numeroAleatorio=6546868;
 		
